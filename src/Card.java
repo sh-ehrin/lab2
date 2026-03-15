e6db4913c76a1b446f2f8314850745ba3452693c
Shehrinh <122511206+Shehrinh@users.noreply.github.com>
2026-03-15 12:43:43 -0400

    completed lab2

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,106 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author mirah
- */
-public class Card {
+* Title: lab2 Project programmer-defined class
+*
+* Description: AP CSA lab 2 project
+- Understand programmer-defined classes
+- Practice testing a programmer-defined class by writing an application class
+- Practice debugging a programmer-defined class
+- Write JavaDoc documentation to explain the individual parts of a programmer-defined class
+*
+* @author Shehrin Habib
+*/
+public class Card{
+    //instance variables
+    private int value; //the number assigned to each card
+    private String suit; //the symbol assigned to each card
+    
+/**
+* parameterized Card constructor
+* the constructor initializes the newly created card using the number sent to it
+* @param num a number between 0 and 51 which will be converted to a value between
+* 1 and 13 and one of the suits
+*/
+    public Card(int num){
+    value = num % 13;
+    if (value == 0){
+        value = 13;
+    }
+    int suitNumber = num / 13;
+    if (suitNumber == 0){
+        suit = new String("clubs");
+    }
+    else if (suitNumber == 1){
+        suit = new String("diamonds");
+    }
+    else if (suitNumber == 2){
+        suit = new String("hearts");
+    }
+    else if (suitNumber == 3){
+        suit = new String("spades");
+    }
+    else
+        suit = new String("ERROR");
+    }
+    
+    public String toString(){
+        if (this.value == 11){
+            return "Jack of " + suit;
+        }
+        else if (this.value == 12){
+            return "Queen of " + suit;
+        }
+        else if (this.value == 13){
+            return "King of " + suit;
+        }
+        else if (this.value == 1){
+            return "Ace of " + suit;
+        }
+        else{
+            return "" + value + " of " + suit;
+        }
+    }
+    
+    public int getValue(){
+        return value;
+    }
+    
+    public String getSuit(){
+        return suit;
+    }
+    //no mutator methods because you never manually change the value or suit of a card.
+    
+    //If a card is valued 11-13 (jack, queen king) return 10 points. else, return number value.
+    public int getPointValue(boolean high){
+        if (value >= 11){
+            return 10;
+        }
+        if (high && value == 1){
+            return 11;
+        }
+        else{
+            return value;
+        }
+    }
+    
+    //check if the values of two cards are equal
+    public boolean equalValue(Card c1){
+        if (this.value == c1.value){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    //checks if the suits of two cards are equal
+    public boolean equalSuit(Card c1){
+        if (this.suit.equals(c1.suit)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
     
 }
--- src/Lab2App.java
+++ src/Lab2App.java
@@ -1,13 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author mirah
- */
+* Title: lab2 Project Application class
+*
+* Description: AP CSA lab 2 project
+- Understand programmer-defined classes
+- Practice testing a programmer-defined class by writing an application class
+- Practice debugging a programmer-defined class
+- Write JavaDoc documentation to explain the individual parts of a programmer-defined class
+*
+* @author Shehrin Habib
+*/
 public class Lab2App {
-    
+    public static void main(String [] args){
+        Card card1 = new Card(28);
+        System.out.println(card1); //instantiated a new card with the value 28. displayed the reference address for card1
+        //value and suit is not displayed because systemout prints the reference address for Objects IF the toString method is not called.
+        
+        int value1 = card1.getValue();
+        String suit1 = card1.getSuit();
+        System.out.println(value1);
+        System.out.println(suit1);
+        //these statements call the accessor methods on card1 and print them
+        
+        int pointValue = card1.getPointValue(false);
+        System.out.println(pointValue);
+        //these statements check the point value of the card. in this case, it is 2 (not J, Q, K).
+        
+        Card card2 = new Card(41);
+        
+        System.out.println("");
+        System.out.println(card1.equalValue(card2)); // checks if the value of card1 == card2
+        System.out.println(card1.equalSuit(card2)); //checks if the suit of card1 == card2
+        System.out.println("The value is the same, the suits are different. \n");
+        
+        //this instantiates 4 new Card objects with different Values and Suits.
+        Card c1 = new Card(14);
+        Card c2 = new Card(26);
+        Card c3 = new Card(38);
+        Card c4 = new Card(50);
+        
+        System.out.println(c1 + "\n" + c2 + "\n" + c3 + "\n" + c4 + "\n");
+        
+        //39-42
+        System.out.println(c1.getPointValue(true));
+        System.out.println(c1.getPointValue(false));
+        System.out.println(c3.getPointValue(true));
+        System.out.println(c3.getPointValue(false));
+        //this code shows the point values for c1 and c3 with aces both high and low
+        
+        if (c3.getValue() < c4.getValue()){
+            System.out.println(c3 + " has a lower value than " + c4);
+        }
+        else{
+            System.out.println(c4 + " has a lower value than " + c3);
+        }
+        //checks which of the cards has a lower value then prints a statement saying which card is lower in value.
+    }
 }
